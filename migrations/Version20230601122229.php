d26c97c85cf2114459b22f221eba1a1c24aa359d
allan pedersen <a.pedersen@free.fr>
2023-06-05 00:39:29 +0200

    recalage migrations

--- migrations/Version20230601122229.php
+++ migrations/Version20230601122229.php
@@ -21,13 +21,11 @@ final class Version20230601122229 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE book_note CHANGE book_paragraph_id book_paragraph_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE book_table ADD CONSTRAINT FK_F8EB9A3F31659B52 FOREIGN KEY (anchor_paragraph_id) REFERENCES book_paragraph (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE book_table DROP FOREIGN KEY FK_F8EB9A3F31659B52');
         $this->addSql('ALTER TABLE book_note CHANGE book_paragraph_id book_paragraph_id INT NOT NULL');
     }
 }
